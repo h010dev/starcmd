74cb3a9097be7f067ba7b4a2616fa9957834c9f2
MohamedAl-Hussein <sn95racing.v6@gmail.com>
2021-07-23 01:09:45 -0400

    Add demo code to fill out initial command views

--- src/starcmd-main-window.c
+++ src/starcmd-main-window.c
@@ -39,6 +39,8 @@ struct _StarcmdMainWindowPrivate
     GtkWidget *grid_commands;
 };
 
+static const gchar *RESOURCE_PATH = "/org/h010dev/starcmd/starcmd-main-window.glade";
+
 G_DEFINE_TYPE_WITH_PRIVATE (StarcmdMainWindow, starcmd_main_window, GTK_TYPE_APPLICATION_WINDOW);
 
 /* METHOD DEFINITIONS */
@@ -50,7 +52,60 @@ starcmd_main_window_populate_widgets (StarcmdMainWindow *self)
 
     for (int i = 0; i < 10; i++) {
         gtk_grid_insert_row (GTK_GRID (priv->grid_commands), i + 1);
-        gtk_grid_attach (GTK_GRID (priv->grid_commands), starcmd_command_view_new (), 1, i + 1, 1, 1);
+        GtkWidget *cmd = starcmd_command_view_new ();
+
+        GValue name = G_VALUE_INIT;
+        g_value_init (&name, G_TYPE_STRING);
+        g_value_set_string (&name, (const gchar *) "Command name");
+        g_object_set_property (G_OBJECT (cmd), "name", &name);
+
+        GValue platform = G_VALUE_INIT;
+        g_value_init (&platform, G_TYPE_STRING);
+        g_value_set_string (&platform, (const gchar *) "Command platform");
+        g_object_set_property (G_OBJECT (cmd), "platform", &platform);
+
+        GValue os = G_VALUE_INIT;
+        g_value_init (&os, G_TYPE_STRING);
+        g_value_set_string (&os, (const gchar *) "Command OS");
+        g_object_set_property (G_OBJECT (cmd), "os", &os);
+
+        GValue desc = G_VALUE_INIT;
+        g_value_init (&desc, G_TYPE_STRING);
+        g_value_set_string (&desc, (const gchar *) "Command description");
+        g_object_set_property (G_OBJECT (cmd), "description", &desc);
+
+        GValue cmds = G_VALUE_INIT;
+        g_value_init (&cmds, G_TYPE_STRING);
+        g_value_set_string (&cmds, (const gchar *) "Command commands");
+        g_object_set_property (G_OBJECT (cmd), "commands", &cmds);
+
+        GValue exs = G_VALUE_INIT;
+        g_value_init (&exs, G_TYPE_STRING);
+        g_value_set_string (&exs, (const gchar *) "Command examples");
+        g_object_set_property (G_OBJECT (cmd), "examples", &exs);
+
+        GValue refs = G_VALUE_INIT;
+        g_value_init (&refs, G_TYPE_STRING);
+        g_value_set_string (&refs, (const gchar *) "Command references");
+        g_object_set_property (G_OBJECT (cmd), "references", &refs);
+
+        GValue datemod = G_VALUE_INIT;
+        g_value_init (&datemod, G_TYPE_STRING);
+        g_value_set_string (&datemod, (const gchar *) "Command date modified");
+        g_object_set_property (G_OBJECT (cmd), "datemod", &datemod);
+
+        GValue icon = G_VALUE_INIT;
+        g_value_init (&icon, G_TYPE_STRING);
+        g_value_set_string (&icon, (const gchar *) "Command icon");
+        g_object_set_property (G_OBJECT (cmd), "icon", &icon);
+
+        GValue fav = G_VALUE_INIT;
+        g_value_init (&fav, G_TYPE_BOOLEAN);
+        g_value_set_boolean (&fav, TRUE);
+        g_object_set_property (G_OBJECT (cmd), "favorite", &fav);
+
+        gtk_grid_attach (GTK_GRID (priv->grid_commands), cmd, 1, i + 1, 1, 1);
+
     }
     
     gtk_widget_show_all (priv->grid_commands);
@@ -59,9 +114,7 @@ starcmd_main_window_populate_widgets (StarcmdMainWindow *self)
 static void
 starcmd_main_window_class_init (StarcmdMainWindowClass *klass)
 {
-    gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass), 
-                                                 "/org/h010dev/starcmd/starcmd-main-window.glade");
-
+    gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (klass), RESOURCE_PATH);
     gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (klass), StarcmdMainWindow, grid_commands);
 }
 
